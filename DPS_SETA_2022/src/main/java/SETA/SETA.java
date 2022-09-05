728290acd3a2f9b4fdbdc146492b716455b6badc
Manuel Pagliuca <pagliuca.manuel@gmail.com>
2022-09-05 13:32:58 +0200

    Refactoring + Comment on the SETA module

--- DPS_SETA_2022/src/main/java/SETA/SETA.java
+++ DPS_SETA_2022/src/main/java/SETA/SETA.java
@@ -12,9 +12,22 @@ import org.eclipse.paho.client.mqttv3.*;
 
 import java.util.*;
 
+/* SETA
+ * ------------------------------------------------------------------------------
+ * This class is a stand-alone process that produces 2 rides every 5 seconds,
+ * these rides will be sent on the relative district (extracted from their starting
+ * position).
+ *
+ * The class is designed in such a way that the rides who haven't got
+ * accomplished will be retained, this through 4 priority queues (one for each district).
+ * The generated ride will be added to the relative priority queue, and then
+ * publish the ride with the highest priority on the topic of the district (except
+ * for a corner case, see comments on 'sendQueuedRide()'
+ */
 public class SETA {
     private static int rideIds = 0;
     private static final int QOS = 2;
+    private final static String BROKER_ADDRESS = "tcp://localhost:1883";
     private final static String COMPLETED_RIDES_TOPIC = "seta/smartcity/rides/completed";
     private static Queue<RideInfo> queueDistrict1;
     private static Queue<RideInfo> queueDistrict2;
@@ -23,61 +36,22 @@ public class SETA {
 
     public static void main(String[] args) {
         org.eclipse.paho.client.mqttv3.MqttClient client;
-        String broker = "tcp://localhost:1883";
         String clientId = org.eclipse.paho.client.mqttv3.MqttClient.generateClientId();
 
         initDistrictQueues();
 
         try {
-            // New client with no persistence of the messages
-            client = new MqttClient(broker, clientId);
+            client = new MqttClient(BROKER_ADDRESS, clientId, null);
             MqttConnectOptions connectOptions = new MqttConnectOptions();
             connectOptions.setCleanSession(true);
 
-            // Connect to client
             client.connect(connectOptions);
 
-            client.setCallback(new MqttCallback() {
-                public void messageArrived(String topic, MqttMessage message) {
-                    if (topic.equals(COMPLETED_RIDES_TOPIC)) {
-                        RideInfo completedRide = Utility.GSON.fromJson(message.toString(), RideInfo.class);
-
-                        if (completedRide.getStartingDistrict() == 1) {
-                            queueDistrict1.remove();
-                        } else if (completedRide.getStartingDistrict() == 2) {
-                            queueDistrict2.remove();
-                        } else if (completedRide.getStartingDistrict() == 3) {
-                            queueDistrict3.remove();
-                        } else if (completedRide.getStartingDistrict() == 4) {
-                            queueDistrict4.remove();
-                        }
-                        //printDistrictQueues();
-                    }
-                }
-
-                public void connectionLost(Throwable cause) {
-                    System.out.println(clientId
-                            + " Connectionlost! cause:"
-                            + cause.getMessage()
-                            + "-  Thread PID: "
-                            + Thread.currentThread().getId());
-                }
-
-                public void deliveryComplete(IMqttDeliveryToken token) {
-
-                }
-            });
+            client.setCallback(getCallback(clientId));
 
             client.subscribe(COMPLETED_RIDES_TOPIC);
+
             rideGeneration(client);
-            // TODO: Insert some thread for handling the reading from console,
-            // todo: so that it is possible to interact with the broker
-            /*
-               System.out.println("\n ***  Press a random key to exit *** \n");
-               Scanner command = new Scanner(System.in);
-               command.nextLine();
-               client.disconnect();
-             */
         } catch (MqttException me) {
             System.out.println("reason " + me.getReasonCode());
             System.out.println("msg " + me.getMessage());
@@ -94,25 +68,54 @@ public class SETA {
         }
     }
 
-    private static void initDistrictQueues() {
-        queueDistrict1 = new PriorityQueue<>(5, (o1, o2) -> compareRideIDs(o1, o2));
-        queueDistrict2 = new PriorityQueue<>(5, (o1, o2) -> compareRideIDs(o1, o2));
-        queueDistrict3 = new PriorityQueue<>(5, (o1, o2) -> compareRideIDs(o1, o2));
-        queueDistrict4 = new PriorityQueue<>(5, (o1, o2) -> compareRideIDs(o1, o2));
+    /* Return the callback for handling the reception of the messages
+     * ------------------------------------------------------------------------------
+     * This process can listen only on the '/seta/smartcity/rides/completed' topic
+     * for handling the recycle of the rides in each queue.
+     *
+     * After a ride will be accomplished the taxi will send a message on this topic
+     * for notify the completion, when the message is received from SETA it will remove
+     * the ride from the relative queue (to the starting district of the ride).
+     */
+    private static MqttCallback getCallback(String clientId) {
+        return new MqttCallback() {
+            public void messageArrived(String topic, MqttMessage message) {
+                if (topic.equals(COMPLETED_RIDES_TOPIC)) {
+                    RideInfo completedRide = Utility.GSON.fromJson(message.toString(), RideInfo.class);
+
+                    if (completedRide.getStartingDistrict() == 1) {
+                        queueDistrict1.remove();
+                    } else if (completedRide.getStartingDistrict() == 2) {
+                        queueDistrict2.remove();
+                    } else if (completedRide.getStartingDistrict() == 3) {
+                        queueDistrict3.remove();
+                    } else if (completedRide.getStartingDistrict() == 4) {
+                        queueDistrict4.remove();
+                    }
+                    //printDistrictQueues(); //debug
+                }
+            }
+
+            public void connectionLost(Throwable cause) {
+                System.out.println(clientId
+                        + " Connectionlost! cause:" + cause.getMessage()
+                        + "-  Thread PID: " + Thread.currentThread().getId());
+            }
+
+            public void deliveryComplete(IMqttDeliveryToken token) {
+
+            }
+        };
     }
 
-    private static int compareRideIDs(RideInfo o1, RideInfo o2) {
-        if (o1.getId() > o2.getId())
-            return 1;
-        else if (o1.getId() < o2.getId())
-            return -1;
-        return 0;
-    }
-
-    /* Ride generation and publishing on the respective topics
+    /* Ride generation and publishing on MQTT district topics
      * ------------------------------------------------------------------------------
      * This method generate 2 rides each 5 seconds on the relative topics (this will
      * depend on the district of the generated rides).
+     *
+     * Then send the rides on the relative district topics, the 'sendQueuedRide()' will
+     * buffer the messages in the district queues and send the message with the highest
+     * priority to the topic.
      */
     @SuppressWarnings("BusyWait")
     private static void rideGeneration(MqttClient client) throws MqttException, InterruptedException {
@@ -120,44 +123,84 @@ public class SETA {
             RideInfo ride1 = generateRide();
             RideInfo ride2 = generateRide();
 
-            // Todo: Trattare il caso in cui entrambe le ride pubblichino sullo stesso distretto
-            boolean sameDistrict = false;
             sendQueuedRide(client, ride1, false);
-
-            if (ride1.getStartingDistrict() == ride2.getStartingDistrict())
-                sameDistrict = true;
-
+            final boolean sameDistrict = ride1.getStartingDistrict() == ride2.getStartingDistrict();
             sendQueuedRide(client, ride2, sameDistrict);
 
             Thread.sleep(5000);
         }
     }
 
-    /*private static void printDistrictQueues() {
-        System.out.println("Queue District 1: " + queueDistrict1);
-        System.out.println("Queue District 2: " + queueDistrict2);
-        System.out.println("Queue District 3: " + queueDistrict3);
-        System.out.println("Queue District 4: " + queueDistrict4);
-    }*/
-
-    private static void sendQueuedRide(MqttClient client, RideInfo ride, boolean sameDistrict) throws MqttException {
+    /* Send the ride on the topic of the starting district of the ride
+     * ------------------------------------------------------------------------------
+     * Since there is a system for buffering the rides which are not accomplished,
+     * the first thing to do is add the ride to the queue of his district.
+     *
+     * If the ride is not in the same district as the previous ride (N.B.: the first
+     * ride will always have this flag set to 'false'), then we must pick
+     * the ride with the highest priority from his district queue.
+     *
+     * Otherwise, if the ride is the same of the previous district we just send that,
+     * otherwise it will keep send the same previous ride (previous because it will
+     * have a smaller ride, so an higher priority).
+     */
+    private static void sendQueuedRide(MqttClient client, RideInfo ride, boolean sameDistrictPrevRide)
+            throws MqttException {
         addRideDistrictQueue(ride);
-        //printDistrictQueues();
+        //printDistrictQueues(); //debug
         MqttMessage mqttMessage;
-        if (!sameDistrict) {
-            // Overwrite with higher priority rides
+        if (!sameDistrictPrevRide)
             ride = pickRideDistrictQueue(ride.getStartingDistrict());
-        }
+
         mqttMessage = new MqttMessage(Utility.GSON.toJson(ride).getBytes());
-
-        //System.out.println("Prioritized " + prioritizedRide.toString() + " from " + ride.getStartingDistrict());
         mqttMessage.setQos(QOS);
-
         String topic1 = "seta/smartcity/rides/district" + ride.getStartingDistrict();
         client.publish(topic1, mqttMessage);
         System.out.println(topic1 + ", " + mqttMessage);
     }
 
+    /* Generate a ride with random positions
+     * ------------------------------------------------------------------------------
+     * The starting and destination positions of the rides are generated in such
+     * a way that they will be in range [0,10]. Also, the destination position will
+     * keep getting regenerated in case it will be equal to the start position.
+     */
+    public static RideInfo generateRide() {
+        RideInfo ride = new RideInfo();
+
+        int[] startPos = generateRandomPosition();
+        int[] destPos = generateRandomPosition();
+        ride.setStartPosition(startPos);
+
+        while (Arrays.equals(destPos, startPos))
+            destPos = generateRandomPosition();
+
+        ride.setDestinationPosition(destPos);
+        ride.setId(++rideIds);
+
+        return ride;
+    }
+
+    /// Utility
+
+    // Generate random position in the range [0,10]
+    private static int[] generateRandomPosition() {
+        Random random = new Random();
+        int[] position = new int[2];
+        position[0] = random.nextInt(0, 10);
+        position[1] = random.nextInt(0, 10);
+        return position;
+    }
+
+    // Prints the content of the queues (debug)
+    private static void printDistrictQueues() {
+        System.out.println("Queue District 1: " + queueDistrict1);
+        System.out.println("Queue District 2: " + queueDistrict2);
+        System.out.println("Queue District 3: " + queueDistrict3);
+        System.out.println("Queue District 4: " + queueDistrict4);
+    }
+
+    // Check the first value of a district queue given the district
     private static RideInfo pickRideDistrictQueue(int startingDistrict) {
         if (startingDistrict == 1)
             return queueDistrict1.peek();
@@ -168,6 +211,7 @@ public class SETA {
         return queueDistrict4.peek();
     }
 
+    // Add a ride to the relative district queue given the ride
     private static void addRideDistrictQueue(RideInfo ride) {
         if (ride.getStartingDistrict() == 1) {
             queueDistrict1.add(ride);
@@ -180,27 +224,20 @@ public class SETA {
         }
     }
 
-    public static RideInfo generateRide() {
-        RideInfo ride = new RideInfo();
+    // Initializes the queue used for the recycling of rides with the comparators
+    private static void initDistrictQueues() {
+        queueDistrict1 = new PriorityQueue<>(5, (o1, o2) -> compareRideIDs(o1, o2));
+        queueDistrict2 = new PriorityQueue<>(5, (o1, o2) -> compareRideIDs(o1, o2));
+        queueDistrict3 = new PriorityQueue<>(5, (o1, o2) -> compareRideIDs(o1, o2));
+        queueDistrict4 = new PriorityQueue<>(5, (o1, o2) -> compareRideIDs(o1, o2));
+    }
 
-        Random random = new Random();
-        int[] startPos = new int[2];
-        startPos[0] = random.nextInt(0, 10);
-        startPos[1] = random.nextInt(0, 10);
-        ride.setStartPosition(startPos);
-
-        int[] destPos = new int[2];
-        destPos[0] = random.nextInt(0, 10);
-        destPos[1] = random.nextInt(0, 10);
-
-        while (Arrays.equals(destPos, startPos)) {
-            destPos[0] = random.nextInt(0, 10);
-            destPos[1] = random.nextInt(0, 10);
-        }
-
-        ride.setDestinationPosition(destPos);
-        ride.setId(++rideIds);
-
-        return ride;
+    // Compare the rides by ride ID, which is incremental (the oldest ride/smaller ID is preferred)
+    private static int compareRideIDs(RideInfo o1, RideInfo o2) {
+        if (o1.getId() > o2.getId())
+            return 1;
+        else if (o1.getId() < o2.getId())
+            return -1;
+        return 0;
     }
 }
--- DPS_SETA_2022/src/main/java/SETA/Structures/RideInfo.java
+++ DPS_SETA_2022/src/main/java/SETA/Structures/RideInfo.java
@@ -6,11 +6,33 @@ package SETA.Structures;
 
 import java.io.Serializable;
 
+/* RideInfo
+ * ------------------------------------------------------------------------------
+ * This class is the structure of ride message, it will be used
+ * for the generation of the rides and for the marshalling/unmarshalling
+ * through Gson (JSON) of the MQTT messages to the broker.
+ *
+ * This will be used both for the sending that for the receiving of the
+ * rides messages (from Taxi and SETA).
+ */
 public class RideInfo implements Serializable {
     private int id;
     private int[] startPosition = new int[2];
     private int[] destinationPosition = new int[2];
 
+    /// Utility
+
+    // Get the starting district of the ride
+    public int getStartingDistrict() {
+        return getDistrict(startPosition);
+    }
+
+    // Get the destination district of the ride
+    public int getDestinationDistrict() {
+        return getDistrict(destinationPosition);
+    }
+
+    // Get the district of the ride given the position, usable only from member functions
     private int getDistrict(int[] position) {
         if (position.length != 2)
             return -1;
@@ -23,14 +45,6 @@ public class RideInfo implements Serializable {
             return 4;
         else
             return 3;
-    }
-
-    public int getStartingDistrict() {
-        return getDistrict(startPosition);
-    }
-
-    public int getDestinationDistrict() {
-        return getDistrict(destinationPosition);
     }
 
     // Getters & Setters
